fba73c94964513620b9fadf04310c26e3800c82e
Stefani Georgievska <43820746+sgeorgievska@users.noreply.github.com>
2019-03-09 01:29:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Conway's Game of Life!</h1>
+      <h1>Conways Game of Life!</h1>
       <Game />
       </div>
     );
